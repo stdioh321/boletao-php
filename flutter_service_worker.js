4846a3c8c2830f6cabc3ef1a63c3128ecea29c74
hdias <diaslinoh@gmail.com>
2020-09-09 17:46:35 -0300

    Update

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -5,7 +5,7 @@ const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "index.html": "614e43e769ff641502d8a7df39ce3715",
 "/": "614e43e769ff641502d8a7df39ce3715",
-"main.dart.js": "644e6a38b56d22473d6b531c9c1ebd0e",
+"main.dart.js": "b006f3dbf40e81f97750b3c6e8b118be",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
